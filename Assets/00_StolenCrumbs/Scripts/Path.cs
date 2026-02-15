7ea0894fa4598ec38dbc7cfa8d52841593989484
DanielGarciaFlorida <dagana@alumnatflorida.es>
2026-02-15 21:29:08 +0100

    Add Path script and prefab
    Created Path script with public pathToFollow list.
    Created Path prefab with updated visual.

---
+++ Assets/00_StolenCrumbs/Scripts/Path.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Path : MonoBehaviour
+{
+    public List<CheckerManager> pathToFollow;
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
