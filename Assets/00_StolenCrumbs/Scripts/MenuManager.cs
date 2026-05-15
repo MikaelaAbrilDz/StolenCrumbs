69db66cecf18c20b3881d63b9fac8b7f26760016
DanielGarcia <dagana@alumnatflorida.es>
2026-05-15 19:27:32 +0200

    Start Menu + Pause Menu improvements + Imports of Sprites

--- Assets/00_StolenCrumbs/Scripts/MenuManager.cs
+++ Assets/00_StolenCrumbs/Scripts/MenuManager.cs
@@ -1,33 +1,15 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject SoundMenuManager;
-    public InputActionReference pauseAction;
-    public void OnEnable()
+    [SerializeField] string gameSceneName;
+    public void PlayGame()
     {
-        pauseAction.action.Enable();
-        pauseAction.action.performed += OnPause;
+        SceneManager.LoadScene(gameSceneName);
     }
-
-    private void OnDisable()
+    public void ExitGame()
     {
-        pauseAction.action.performed -= OnPause;
-        pauseAction.action.Disable();
-    }
-    
-    private void OnPause(InputAction.CallbackContext context)
-    {
-        if (SoundMenuManager.activeSelf)
-        {
-            SoundMenuManager.SetActive(false);
-            Time.timeScale = 1f;
-        }
-        else
-        {
-            SoundMenuManager.SetActive(true);
-            Time.timeScale = 0f;
-        }
+        Application.Quit();
     }
 }
---
+++ Assets/00_StolenCrumbs/Scripts/SoundsMenuManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SoundsMenuManager : MonoBehaviour
+{
+    public GameObject SoundMenuManager;
+    public InputActionReference pauseAction;
+
+    [SerializeField] string gameSceneName;
+    public void OnEnable()
+    {
+        pauseAction.action.Enable();
+        pauseAction.action.performed += OnPause;
+    }
+
+    private void OnDisable()
+    {
+        pauseAction.action.performed -= OnPause;
+        pauseAction.action.Disable();
+    }
+    
+    private void OnPause(InputAction.CallbackContext context)
+    {
+        if (SoundMenuManager.activeSelf)
+        {
+            SoundMenuManager.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            SoundMenuManager.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
